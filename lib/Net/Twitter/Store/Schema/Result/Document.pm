a693993eb6839461d87f6e8ea1a4f77b5b22460d
manu <manu@edgeoya.(none)>
2010-02-17 10:43:22 +0100

    Document.pm: add a class to handle the documents table

---
+++ lib/Net/Twitter/Store/Schema/Result/Document.pm
@@ -0,0 +1,43 @@
+package Net::Twitter::Store::Schema::Result::Document;
+
+use base 'DBIx::Class';
+
+__PACKAGE__->load_components(qw/ Core /);
+__PACKAGE__->table('documents');
+
+__PACKAGE__->add_columns(
+    'id' => {
+        'data_type'         => 'bigint',
+        'is_auto_increment' => 1,
+        'default_value'     => undef,
+        'is_foreign_key'    => 0,
+        'name'              => 'id',
+        'is_nullable'       => 0,
+        'size'              => '20'
+    },
+    'name' => {
+        'data_type'         => 'varchar',
+        'is_auto_increment' => 0,
+        'default_value'     => undef,
+        'is_foreign_key'    => 0,
+        'name'              => 'name',
+        'is_nullable'       => 0,
+        'size'              => '255'
+    },
+    'type' => {
+        'data_type'         => 'varchar',
+        'is_auto_increment' => 0,
+        'default_value'     => undef,
+        'is_foreign_key'    => 0,
+        'name'              => 'type',
+        'is_nullable'       => 1,
+        'size'              => '255'
+    },
+);
+
+__PACKAGE__->set_primary_key('id');
+__PACKAGE__->add_unique_constraint( 'name', ['name'] );
+__PACKAGE__->has_many(
+    properties => 'Net::Twitter::Store::Schema::Result::Property' );
+
+1;    # End of Net::Twitter::Store::Schema
